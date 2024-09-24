3d8ccef27a34c70b1e1872fe970f4121f9af3c40
Claudé Davis <claudedavis2024@gmail.com>
2024-09-24 09:12:06 -0400

    Add files via upload

---
+++ pyGame2.py
@@ -0,0 +1,21 @@
+# file name: Visual-Notes_0.py
+
+import pygame
+
+pygame.init()        # initializes Pygame
+
+screen = pygame.display.set_mode((500,500))     # initializes the Display(def. what will be shown on the screen) and its resolution (in this case 500x500), and stores it in a variable
+
+surf = pygame.Surface((200,200))        # establishes the size of the surface ((width, height))
+surf.fill(color=(255,0,255))      # COLORING; creates a purple square in the top left corner of the 
+
+screen.blit(surf, (0,0))          # BLITTING; copies the pixels/information from variable 'surf' to variable 'screen' onto the (0,0) coordinates
+pygame.display.flip()            # tells the computer to update the whole screen
+
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
