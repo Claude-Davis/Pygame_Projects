4bc6cf524f452c0c7de963b613771079e183e757
Claudé Davis <claudedavis2024@gmail.com>
2024-09-24 09:11:41 -0400

    Update and rename Pygame_Projects_0.py to Pygame-Projects_Notes-0.py

--- Pygame-Projects_Notes-0.py
+++ Pygame-Projects_Notes-0.py
@@ -1,7 +1,7 @@
 INSTALLATION
 * https://www.pygame.org/
 -----------------------------------------------------------------------------------------------------------------
-NEW PROJECT
+STARTING A NEW PROJECT
   import pygame
 -----------------------------------------------------------------------------------------------------------------
 EXAMPLE GAMES
@@ -26,10 +26,10 @@ EXAMPLE GAMES
     import pygame.examples.playmus as pm
     pm.main("hymn-to-aurora.ogg")
 -----------------------------------------------------------------------------------------------------------------
-ABOUT / INCLUDED
+ABOUT / INCLUDED IN PYGAME
 * Built on top of the SDL (Simple Direct Media) to access audio, keyboar, mouse and graphics hardware.
 -----------------------------------------------------------------------------------------------------------------
-* EVENTS
+EVENTS
     import pygame
     import pygame.examples.eventlist as ev
     ev.main()
@@ -39,4 +39,25 @@ STRUCTURE OF PYGAME APPLICATION
 * Advised to start with a template.
 * MODULES: Load any modules your game needs.
 * EXTERNAL RESOURCES: Manage external resources. Load any sounds or images. If your game is played over the network, or it supports saving and loading from the file, this is where you'd handle it.
-
+-----------------------------------------------------------------------------------------------------------------
+BLITTING
+* Old technical term (Bit Block Transfer)
+* def. copying over all pixels from one surface to another. / The transfer of bits.
+* The computer copies over all of the pixels in/from 'Surf' into 'Screen'
+  - refer to file <Visual-Notes_0.py>
+* GENERAL METHOD:
+    target_surface.blit(Surface, (x,y))
+  - Breakdown:
+    - "target_surface": where we want to draw to
+    - "Surface": where the information is coming from
+    - "(x,y)": the coordinates for where on the target the source should be drawn
+-----------------------------------------------------------------------------------------------------------------
+UPDATING THE SCREEN
+* Updating the program by adding a command relating the screen **only updates the Display's Surface**
+* The computer must be told to update, as well, using either of the methods below:
+  - pygame.display.flip() 
+    - updates the whole screen
+  - pygame.display.update() 
+    - updates parts of the screen
+* You will generally use the   update()   method due to the fact that updating the entire screen can take a lot of time and may cause lag.
+   - The lag is seen especially when updating larger screens
