877a0aa2a374af150a7e86a58a34998d77027f39
Claudé Davis <claudedavis2024@gmail.com>
2024-09-30 20:27:12 -0400

    Create 14.Example-Notes-7_Screen-Color-Changing.py

---
+++ 14.Example-Notes-7_Screen-Color-Changing.py
@@ -0,0 +1,32 @@
+# Loop: Gradually changes the color of the screen from black to white, and then back to black
+
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((400,400))
+
+color_value = 0
+increasing = True  # determines if the color value is increasing or decreasing
+
+run = True
+while run:
+    # fills the screen with the current color
+    screen.fill((color_value,color_value,color_value))
+
+    if increasing:
+        color_value += 1
+        if color_value >=255:
+            increasing = False
+    else:
+        color_value -= 1
+        if color_value <= 0:
+            increasing = True
+
+    pygame.display.flip()
+    clock.tick(85)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
